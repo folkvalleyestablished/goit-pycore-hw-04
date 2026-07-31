b9436bc33015a5e538361883136e62fa0115fd16
folkvalleyestablished <onethree2223@gmail.com>
2026-07-31 19:25:16 +0200

    -

---
+++ hw04.py
@@ -0,0 +1,79 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+
+def change_contact(args, contacts):
+    name, phone = args
+    if name not in contacts:
+        return "Contact not found."
+    contacts[name] = phone
+    return "Contact updated."
+
+
+def show_phone(args, contacts):
+    name = args[0]
+    if name not in contacts:
+        return "Contact not found."
+    return contacts[name]
+
+
+def show_all(contacts):
+    if not contacts:
+        return "No contacts yet."
+
+    result = ""
+    for name, phone in contacts.items():
+        result += f"{name}: {phone}\n"
+    return result.strip()
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+
+        elif command == "add":
+            try:
+                print(add_contact(args, contacts))
+            except ValueError:
+                print("Invalid command.")
+
+        elif command == "change":
+            try:
+                print(change_contact(args, contacts))
+            except ValueError:
+                print("Invalid command.")
+
+        elif command == "phone":
+            try:
+                print(show_phone(args, contacts))
+            except IndexError:
+                print("Invalid command.")
+
+        elif command == "all":
+            print(show_all(contacts))
+
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
